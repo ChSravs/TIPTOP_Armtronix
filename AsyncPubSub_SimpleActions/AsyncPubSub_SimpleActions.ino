f2651d12b361c8bc8943cf4eb10baf853739638d
ChSravs <sravya.amicus@gmail.com>
2019-02-14 15:33:45 +0530

    Basic Actions

---
+++ AsyncPubSub_SimpleActions/AsyncPubSub_SimpleActions.ino
@@ -0,0 +1,127 @@
+#include <ESP8266WiFi.h>
+#include <Ticker.h>
+#include <AsyncMqttClient.h>
+#include <ArduinoJson.h>
+
+#define WIFI_SSID "TIPTOP"
+#define WIFI_PASSWORD "tiptop@mail1"
+
+#define MQTT_HOST "tiptopdrive.com"
+#define MQTT_PORT 1883
+
+AsyncMqttClient mqttClient;
+Ticker mqttReconnectTimer;
+
+WiFiEventHandler wifiConnectHandler;
+WiFiEventHandler wifiDisconnectHandler;
+Ticker wifiReconnectTimer;
+
+void connectToWifi() {
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+}
+
+void onWifiConnect(const WiFiEventStationModeGotIP& event) {
+  Serial.println("Connected to Wi-Fi.");
+  connectToMqtt();
+}
+
+void onWifiDisconnect(const WiFiEventStationModeDisconnected& event) {
+  Serial.println("Disconnected from Wi-Fi.");
+  mqttReconnectTimer.detach(); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
+  wifiReconnectTimer.once(2, connectToWifi);
+}
+
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+}
+
+void onMqttConnect(bool sessionPresent) {
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+  uint16_t packetIdSub = mqttClient.subscribe("dev/1231231231", 2);
+  Serial.print("Subscribing at QoS 2, packetId: ");
+  Serial.println(packetIdSub);
+  mqttClient.publish("test/lol", 0, true, "test 1");
+  Serial.println("Publishing at QoS 0");
+  uint16_t packetIdPub1 = mqttClient.publish("dev/1231231231", 1, true, "Armtronix");
+  Serial.print("Publishing at QoS 1, packetId: ");
+  Serial.println(packetIdPub1);
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+  Serial.println("Disconnected from MQTT.");
+
+  if (WiFi.isConnected()) {
+    mqttReconnectTimer.once(2, connectToMqtt);
+  }
+}
+
+void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
+  Serial.println("Subscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+  Serial.print("  qos: ");
+  Serial.println(qos);
+}
+
+void onMqttUnsubscribe(uint16_t packetId) {
+  Serial.println("Unsubscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+  if(properties.retain == 0){
+    Serial.println(payload);
+    const size_t capacity = JSON_OBJECT_SIZE(10);
+    DynamicJsonBuffer jsonBuffer(capacity);
+    JsonObject& data = jsonBuffer.parseObject(payload);
+    String action = data["action"]; // "update"
+
+    if(action.equals("Switch")){
+      String switchId = data["switchId"].as<String>(); //"1"
+      int switchState = data["switchState"];
+      if(switchState){
+        Serial.print("R_");
+        Serial.print(switchId);
+        Serial.println(" switched via web request to 1");
+      }
+      else{
+        Serial.print("R_");
+        Serial.print(switchId);
+        Serial.println(" switched via web request to 0");
+      }
+    }
+  }
+}
+
+void onMqttPublish(uint16_t packetId) {
+  Serial.println("Publish acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println();
+
+  wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
+  wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
+
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.onDisconnect(onMqttDisconnect);
+  mqttClient.onSubscribe(onMqttSubscribe);
+  mqttClient.onUnsubscribe(onMqttUnsubscribe);
+  mqttClient.onMessage(onMqttMessage);
+  mqttClient.onPublish(onMqttPublish);
+  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+
+  connectToWifi();
+}
+
+void loop() {
+}
---
+++ SimpleActions/SimpleActions.ino
@@ -0,0 +1,42 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+}
+
+void loop() {
+  Serial.println("R_1 switched via web request to 1");
+  delay(1000);
+  Serial.println("R_2 switched via web request to 1");
+  delay(1000);
+  Serial.println("R_3 switched via web request to 1");
+  delay(1000);
+  Serial.println("R_4 switched via web request to 1");
+  delay(1000);
+  Serial.println("R_5 switched via web request to 1");
+  delay(1000);
+  Serial.println("R_6 switched via web request to 1");
+  delay(1000);
+  Serial.println("R_7 switched via web request to 1");
+  delay(1000);
+  Serial.println("R_8 switched via web request to 1");
+  delay(1000);
+  Serial.println("R_1 switched via web request to 0");
+  delay(1000);
+  Serial.println("R_2 switched via web request to 0");
+  delay(1000);
+  Serial.println("R_3 switched via web request to 0");
+  delay(1000);
+  Serial.println("R_4 switched via web request to 0");
+  delay(1000);
+  Serial.println("R_5 switched via web request to 0");
+  delay(1000);
+  Serial.println("R_6 switched via web request to 0");
+  delay(1000);
+  Serial.println("R_7 switched via web request to 0");
+  delay(1000);
+  Serial.println("R_8 switched via web request to 0");
+  delay(1000);
+  // put your main code here, to run repeatedly:
+
+}
